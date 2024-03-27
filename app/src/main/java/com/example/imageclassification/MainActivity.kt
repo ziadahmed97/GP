c190c14c8583b47d84c1e1ec6f7b3ed810405347
ziadahmed97 <zizoaziz2002@gmail.com>
2024-03-27 04:23:37 +0200

    DataPassing

--- app/src/main/java/com/example/imageclassification/MainActivity.kt
+++ app/src/main/java/com/example/imageclassification/MainActivity.kt
@@ -5,6 +5,8 @@ import android.graphics.Bitmap
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.provider.MediaStore
 import com.example.imageclassification.databinding.ActivityMainBinding
 import com.example.imageclassification.ml.MobilenetV110224Quant
@@ -13,12 +15,27 @@ import org.tensorflow.lite.support.image.ImageProcessor
 import org.tensorflow.lite.support.image.TensorImage
 import org.tensorflow.lite.support.image.ops.ResizeOp
 import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
+import java.util.*
+import android.speech.tts.TextToSpeech
+import android.widget.Toast
+import androidx.core.content.ContextCompat.startActivity
+import androidx.core.os.bundleOf
+import java.io.File
+import java.io.FileOutputStream
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private var bitmap: Bitmap ?= null
     private val CAPTURE_IMAGE_REQUEST = 1
     private val SELECT_IMAGE_REQUEST = 2
+    private lateinit var textToSpeech: TextToSpeech
+    private val resultsList = ArrayList<Pair<File, File>>()
+    private val handler = Handler(Looper.getMainLooper())
+    private val runnable = object : Runnable {
+        override fun run() {
+            binding.camera.performClick()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,32 +53,73 @@ class MainActivity : AppCompatActivity() {
             intent.setType("image/*")
             startActivityForResult(intent, SELECT_IMAGE_REQUEST)
         }
+        binding.btnSave.setOnClickListener{
+            if (bitmap != null && binding.textView.text.isNotEmpty()) {
+                val timestamp = System.currentTimeMillis()
+                val imageFile = File(this.filesDir, "imageFile_$timestamp")
+                val fos = FileOutputStream(imageFile)
+                bitmap!!.compress(Bitmap.CompressFormat.PNG, 100, fos)
+                fos.close()
+
+                val textFile = File(this.filesDir, "textFile_$timestamp")
+                textFile.writeText(binding.textView.text.toString())
+
+                resultsList.add(Pair(imageFile, textFile))
+                Toast.makeText(this, "Result saved", Toast.LENGTH_SHORT).show()
+                handler.postDelayed(runnable, 5000)
+            } else {
+                Toast.makeText(this, "No result to save", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        binding.btnViewSaved.setOnClickListener {
+            val intent = Intent(this, SavedImagesActivity::class.java)
+            intent.putExtra("results", resultsList)
+            startActivity(intent)
+        }
+
 
         val labels = application.assets.open("labels.txt").bufferedReader().readLines()
 
         val imageProcessor = ImageProcessor.Builder()
             .add(ResizeOp(224,224, ResizeOp.ResizeMethod.BILINEAR)).build()
 
+        textToSpeech = TextToSpeech(applicationContext) { status ->
+            if (status != TextToSpeech.ERROR) {
+                textToSpeech.language = Locale.US
+            }
+        }
         binding.btnPredict.setOnClickListener{
-            val tensorImage = TensorImage(DataType.UINT8)
-            tensorImage.load(bitmap)
-            val processedImage = imageProcessor.process(tensorImage)
+            if(bitmap != null) {
+                val tensorImage = TensorImage(DataType.UINT8)
+                tensorImage.load(bitmap)
+                val processedImage = imageProcessor.process(tensorImage)
 
-            val model = MobilenetV110224Quant.newInstance(this)
+                val model = MobilenetV110224Quant.newInstance(this)
 
-            val inputFeature0 = TensorBuffer.createFixedSize(intArrayOf(1, 224, 224, 3), DataType.UINT8)
-            inputFeature0.loadBuffer(processedImage.buffer)
+                val inputFeature0 =
+                    TensorBuffer.createFixedSize(intArrayOf(1, 224, 224, 3), DataType.UINT8)
+                inputFeature0.loadBuffer(processedImage.buffer)
 
-            val outputs = model.process(inputFeature0)
-            val outputFeature0 = outputs.outputFeature0AsTensorBuffer.floatArray
+                val outputs = model.process(inputFeature0)
+                val outputFeature0 = outputs.outputFeature0AsTensorBuffer.floatArray
 
-            var maxIdx = outputFeature0.indices.maxByOrNull { outputFeature0[it] } ?: -1
+                var maxIdx = outputFeature0.indices.maxByOrNull { outputFeature0[it] } ?: -1
 
-            binding.textView.setText(labels[maxIdx])
+                val prediction = labels[maxIdx]
 
-            model.close()
+                binding.textView.setText(labels[maxIdx])
+
+                textToSpeech.speak(prediction, TextToSpeech.QUEUE_FLUSH, null, "")
+                model.close()
+
+            }else {
+                // If there is no image, display a message
+                Toast.makeText(this, "No image to predict", Toast.LENGTH_SHORT).show()
+            }
         }
     }
+
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
@@ -78,5 +136,14 @@ class MainActivity : AppCompatActivity() {
             }
             binding.ivImage.setImageBitmap(bitmap)
         }
+    }
+
+    override fun onDestroy() {
+        if (textToSpeech.isSpeaking) {
+            textToSpeech.stop()
+        }
+        textToSpeech.shutdown()
+        super.onDestroy()
+        handler.removeCallbacks(runnable)
     }
 }
--- app/src/main/java/com/example/imageclassification/SavedImagesActivity.kt
+++ app/src/main/java/com/example/imageclassification/SavedImagesActivity.kt
@@ -1,11 +1,58 @@
 package com.example.imageclassification
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.imageclassification.databinding.ActivitySavedImagesBinding
+import java.io.File
 
 class SavedImagesActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySavedImagesBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_saved_images)
+        binding = ActivitySavedImagesBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val resultsList: ArrayList<Pair<File, File>> = intent.getSerializableExtra("results") as ArrayList<Pair<File, File>>
+
+        // Set the layout manager and adapter for the RecyclerView
+        binding.RV.layoutManager = LinearLayoutManager(this)
+        binding.RV.adapter = ResultsAdapter(resultsList)
     }
+}
+
+class ResultsAdapter(private val results: List<Pair<File, File>>) : RecyclerView.Adapter<ResultsAdapter.ResultViewHolder>() {
+
+    class ResultViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val imageView:ImageView
+        val textView: TextView
+        init {
+            imageView = view.findViewById(R.id.itemImage)
+            textView = view.findViewById(R.id.itemText)
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ResultViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_view, parent, false)
+        return ResultViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ResultViewHolder, position: Int) {
+        val result = results[position]
+        val bitmap = BitmapFactory.decodeFile(result.first.path)
+        val text = result.second.readText()
+        holder.imageView.setImageBitmap(bitmap)
+        holder.textView.text = text
+    }
+
+    override fun getItemCount() = results.size
 }

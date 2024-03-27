789e13e433447816ce4026b69c1170bbd6d8cc9a
ziadahmed97 <zizoaziz2002@gmail.com>
2024-03-27 04:15:36 +0200

    Image&TextPassing

---
+++ app/src/main/java/com/example/imageclassification/SavedImagesActivity.kt
@@ -0,0 +1,11 @@
+package com.example.imageclassification
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SavedImagesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_saved_images)
+    }
+}
